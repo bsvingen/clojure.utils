919721bae14eca8d72a237c28c9530e9e45af7ee
Børge Svingen <bsvingen@borkdal.com>
2015-01-25 21:32:56 +0100

    Increasing version to 0.1.1-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject com.borkdal/clojure.utils "0.1.0"
+(defproject com.borkdal/clojure.utils "0.1.1-SNAPSHOT"
   :description "Clojure utilities."
   :url "https://github.com/bsvingen/clojure.utils"
   :license {:name "Apache License 2.0"
