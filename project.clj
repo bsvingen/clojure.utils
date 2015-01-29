c6f814eb14803d18927e7992b9d4cb7fef833ec2
Børge Svingen <bsvingen@borkdal.com>
2015-01-29 21:12:48 +0100

    Adding Clojars to project.clj

--- project.clj
+++ project.clj
@@ -7,4 +7,5 @@
   :plugins [[midje-readme "1.0.7"]]
   :profiles {:dev {:dependencies [[midje "1.6.3" :exclusions [joda-time]]
                                   [clj-time "0.9.0"]]
-                   :plugins [[lein-midje "3.1.3"]]}})
+                   :plugins [[lein-midje "3.1.3"]]}}
+  :deploy-repositories [["releases" :clojars]])
