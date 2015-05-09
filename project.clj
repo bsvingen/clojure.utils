ab2bcf49aa46b0c4f51ca1ace339eff294efd68d
Børge Svingen <bsvingen@borkdal.com>
2015-05-09 22:36:37 +0200

    Version 0.1.2

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject com.borkdal/clojure.utils "0.1.2-SNAPSHOT"
+(defproject com.borkdal/clojure.utils "0.1.2"
   :description "Clojure utilities."
   :url "https://github.com/bsvingen/clojure.utils"
   :license {:name "Apache License 2.0"
